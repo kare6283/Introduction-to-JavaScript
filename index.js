88d7b5aa527532c7ef3eeeef0a77222b8dd49383
Michael Karel <kare6283@gmail.com>
2021-06-21 07:36:10 -0600

    completed task 5b

--- index.js
+++ index.js
@@ -213,11 +213,11 @@ Using the feet function below do the following:
   3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-    /*add your code here*/
+function feet(cm){
+    return cm / 30.48;
   }
  
-
+console.log('task 5b', feet(42));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
